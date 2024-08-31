9e68ed716ca22ddcaeae946c5fc08710e08e340d
Leonardo Bonifácio <estudotrafego@outlook.com>
2024-08-31 14:25:12 -0300

    Esqueci dos tipos sanguineos

--- src/Medico.java
+++ src/Medico.java
@@ -4,7 +4,7 @@ public class Medico extends Usuario {
     private Especialidade especialidade;
     private String crm;
     
-    public Medico(long id, String nome, long cpf, long rg, Date dataNascimento, Endereco endereco,
+    public Medico(String nome, long cpf, long rg, Date dataNascimento, Endereco endereco,
             String telefone, Especialidade especialidade, String crm) {
         super(nome, cpf, rg, dataNascimento, endereco,telefone);
         this.especialidade = especialidade;
