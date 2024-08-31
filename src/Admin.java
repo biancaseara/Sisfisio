3257da729b77d3f93406b5e26f4e5acf43078dc9
biancaseara <biancaseara.p@gmail.com>
2024-08-31 19:05:30 -0300

    Adicionei alguns métodos no Admin.

--- src/Admin.java
+++ src/Admin.java
@@ -1,13 +1,52 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Admin extends Usuario {
+    private List<Usuario> usuarios;
 
     public Admin(String nome, long cpf, long rg, Date dataNascimento, Endereco endereco,
             String telefone) {
         super(nome, cpf, rg, dataNascimento, endereco, telefone);
+        this.usuarios = new ArrayList<>();
+        this.tornaModerador();
     }
     
     public void criarUsuario(Usuario usuario) {
-        
+        usuarios.add(usuario);
+        System.out.println("Usuário criado com sucesso!");
     }
+
+    public void deletarUsuario(long id) {
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getId() == id) {
+                // tem que checar se desejam mesmo remover 
+                usuarios.remove(i);
+                System.out.println();
+                return;
+            }
+        }
+        System.out.println("Usuário com id " + id + " não encontrado.");
+    }
+
+    public Usuario buscarUsuario(long id) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getId() == id) {
+                return usuario;
+            }
+        }
+        System.out.println("Usuário com id " + id + " não encontrado.");
+        return null;
+    }
+
+    public void listarUsuarios() {
+        if (usuarios.isEmpty()) {
+            System.out.println("Nenhum usuário cadastrado.");
+        } else {
+            for (Usuario usuario : usuarios) {
+                System.out.println("ID: " + usuario.getId() + ", Nome: " + usuario.getNome());
+            }
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +9,27 @@ public class Main {
         historicoFamiliar.porcentagemEmRelacaoAoSexo();
         historicoFamiliar.tabelaDasIdadesAfetadas();
         historicoFamiliar.tabelaDasComorbidadesFamiliares();
+
+        Date date = new Date();
+        Endereco endereco = new Endereco("12345-678", "Rua 1", "Bairro 1", "Compl.1", 123);
+        Admin admin = new Admin("Admin", 12345678901L, 12345678L, date, endereco, "12345678901");
+        String[] alergias = {"Pólen", "Amendoim"};
+        String[] comorbidades = {"Hipertensão"};
+        String[] medicamentos = {"Aspirina"};
+        Paciente paciente = new Paciente("Paciente 1", 11111111111L, 11111111L, date, endereco, "10100001111", TipoSanguineo.O_POSITIVO, alergias, comorbidades, medicamentos, historicoFamiliar);
+        Paciente paciente2 = new Paciente("Paciente 2", 22222222222L, 22222222L, date, endereco, "10100002222", TipoSanguineo.O_POSITIVO, alergias, comorbidades, medicamentos, historicoFamiliar);
+        
+        admin.listarUsuarios();
+
+        admin.criarUsuario(paciente);
+        admin.criarUsuario(paciente2);
+
+        admin.listarUsuarios();
+
+        Usuario usuarioId = admin.buscarUsuario(paciente.getId());
+        System.out.println("Usuário encontrado: " + usuarioId.getNome());
+
+        admin.deletarUsuario(paciente.getId());
+        admin.listarUsuarios();
     }
 }
